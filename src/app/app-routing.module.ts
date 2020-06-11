dbf40e71dc7f4c65fa2ad9762b95cf63507cce96
hardeeptech <o7emp04@gmail.com>
2020-06-11 17:08:47 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { TablesPageComponent } from './tables-page/tables-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'home', component: DashboardPageComponent},
+  { path: 'tables', component: TablesPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarNavigationComponent } from './sidebar-navigation/sidebar-navigation.component';
+import { TopNavigationComponent } from './top-navigation/top-navigation.component';
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { TablesPageComponent } from './tables-page/tables-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarNavigationComponent,
+    TopNavigationComponent,
+    DashboardPageComponent,
+    TablesPageComponent
   ],
   imports: [
     BrowserModule,
